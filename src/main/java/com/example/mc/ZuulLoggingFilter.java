07fa07b77ce0767df8b7a38b7abbff860b3d14a1
Sekar <605750@cognizant.com>
2020-08-31 18:30:26 +0530

    created api gateway service

---
+++ src/main/java/com/example/mc/ZuulLoggingFilter.java
@@ -0,0 +1,46 @@
+package com.example.mc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class ZuulLoggingFilter extends ZuulFilter{
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext ctx = RequestContext.getCurrentContext();
+
+		HttpServletRequest req = ctx.getRequest();
+
+		log.info(String.format("%s Request to %s", req.getMethod(), req.getRequestURL()));
+
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+}
